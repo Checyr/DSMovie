ae6a7ec1a3d58e9c06fad8dcb1a58bb357da109f
Checyr <gabrielly.checyr@gmail.com>
2022-03-13 18:28:24 -0300

    Routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import Navbar from 'assets/components/navbar';
+import Navbar from 'components/navbar';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return(
+        <h1>Pagina De Listagem</h1>
+    );
+}
+
+export default Listing;
