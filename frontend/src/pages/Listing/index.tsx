2ef0ad5c721bad30bc228b1a4506032260aa2559
Checyr <gabrielly.checyr@gmail.com>
2022-03-13 19:21:15 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,30 @@
 import './style.css';
+import MovieScore from 'components/MovieScore';
 import Pagination from "components/Pagination";
+import MovieCard from 'components/MovieCard';
 
 function Listing(){
     return(
-        <Pagination/>
+        <>
+                <Pagination/>
+
+                <div className='row'>
+                    <div className='col-sm-6 col-lg-3 col-xl-3'>
+                            <MovieCard/>
+                    </div>
+                    <div className='col-sm-6 col-lg-3 col-xl-3'>
+                            <MovieCard/>
+                    </div>
+                    <div className='col-sm-6 col-lg-3 col-xl-3'>
+                            <MovieCard/>
+                    </div>
+                    <div className='col-sm-6 col-lg-3 col-xl-3'>
+                            <MovieCard/>
+                    </div>
+                </div>
+
+        </>
+
     );
 }
 
